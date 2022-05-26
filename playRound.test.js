9beff50b58dec0157921c12ca97110d2042c1eca
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-05-26 11:20:25 +0200

    Add test for calculateOutcome

---
+++ playRound.test.js
@@ -0,0 +1,46 @@
+const calculateOutcome = require('./script');
+
+describe('calculateOutCome (rock)', () => {
+    test('rock vs rock', () => {
+        expect(calculateOutcome('rock', 'rock'))
+            .toEqual('draw');
+    });
+    test('rock vs scissors', () => {
+        expect(calculateOutcome('rock', 'scissors'))
+            .toEqual('win');
+    });
+    test('rock vs rock', () => {
+        expect(calculateOutcome('rock', 'paper'))
+            .toEqual('lose');
+    });
+})
+
+describe('calculateOutCome (paper)', () => {
+    test('rock vs rock', () => {
+        expect(calculateOutcome('paper', 'rock'))
+            .toEqual('win');
+    });
+    test('rock vs scissors', () => {
+        expect(calculateOutcome('paper', 'scissors'))
+            .toEqual('lose');
+    });
+    test('rock vs rock', () => {
+        expect(calculateOutcome('paper', 'paper'))
+            .toEqual('draw');
+    });
+})
+
+describe('calculateOutCome (scissors)', () => {
+    test('rock vs rock', () => {
+        expect(calculateOutcome('scissors', 'rock'))
+            .toEqual('lose');
+    });
+    test('rock vs scissors', () => {
+        expect(calculateOutcome('scissors', 'scissors'))
+            .toEqual('draw');
+    });
+    test('rock vs rock', () => {
+        expect(calculateOutcome('scissors', 'paper'))
+            .toEqual('win');
+    });
+})
--- script.js
+++ script.js
@@ -44,3 +44,5 @@ Returns random integer between min (inclusive) and max (exclusive);
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+module.exports = calculateOutcome;
