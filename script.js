53d27ff8add4773954c766d029e180a822b4ff20
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-05-26 21:53:54 +0200

    Change gameplay to use user input via buttons

--- script.js
+++ script.js
@@ -25,24 +25,46 @@ function calculateOutcome(playerSelection, computerSelection) {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    const outcome = calculateOutcome(playerSelection.toLowerCase(), computerSelection);
-    return `You ${outcome}, ${playerSelection.toLowerCase()} vs ${computerSelection}.`;
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerInput = prompt("Rock, paper, or scissors?");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerInput, computerSelection));
-    }
-}
-
 /*
 Returns random integer between min (inclusive) and max (exclusive);
 */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+function initializeListeners() {
+    const btns = document.querySelectorAll('.btn');
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const playerSelection = e.target.innerHTML;
+            const outcome = playRound(playerSelection);
+            const resultText = document.querySelector('.round-outcome > p');
+            resultText.innerHTML = outcome;
+        })
+    })
+}
+
+const updateScore = (function() {
+    let playerScore = 0;
+    let computerScore = 0;
+    const gameOutcome = document.querySelector('.game-outcome > p');
+
+    return function(hasPlayerWon) {
+        hasPlayerWon ? playerScore++ : computerScore++;
+        gameOutcome.innerHTML = `${playerScore} vs ${computerScore}`;
+        console.log(gameOutcome);
+    }
+})();
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    const outcome = calculateOutcome(playerSelection.toLowerCase(), computerSelection);
+    if (outcome !== 'draw') {
+        outcome === 'win' ? updateScore(true) : updateScore(false);
+    }
+    return `You ${outcome}, ${playerSelection.toLowerCase()} vs ${computerSelection}.`;
+}
+
+window.onload = initializeListeners();
 
 export {calculateOutcome}
