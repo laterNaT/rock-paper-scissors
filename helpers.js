1caafa65b7822cb9a840201ed8c04d270bb4441c
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-05-27 20:58:08 +0200

    Refactor code

--- helpers.js
+++ helpers.js
@@ -1,51 +1,57 @@
 function toggleButtons(toDisable) {
-    const btns = document.querySelectorAll('.btn');
-    btns.forEach((btn) => {
-        btn.disabled = toDisable;
-    })
+  const btns = document.querySelectorAll('.btn');
+  btns.forEach((btn) => {
+    btn.disabled = toDisable;
+  });
 }
 
 function generateEmoji(name) {
-    const btn = document.createElement('button');
-    const span = document.createElement('span');
-    btn.classList.add('btn2');
-    btn.appendChild(span);
-    switch (name) {
-        case 'rock':
-            span.innerHTML = '✊';
-            return btn;
-        case 'paper':
-            span.innerHTML = '✋';
-            return btn;
-        case 'scissors':
-            span.innerHTML = '✌️';
-            return btn;
-        default:
-            return;
-    }
+  const btn = document.createElement('button');
+  const span = document.createElement('span');
+  btn.classList.add('btn2');
+  btn.appendChild(span);
+  switch (name) {
+    case 'rock':
+      span.innerHTML = '✊';
+      break;
+    case 'paper':
+      span.innerHTML = '✋';
+      break;
+    case 'scissors':
+      span.innerHTML = '✌️';
+      break;
+    default:
+      return btn;
+  }
+  return btn;
 }
 
 /*
 Returns random integer between min (inclusive) and max (exclusive);
 */
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function initializeText() {
-    const score = document.querySelector('.score > p');
-    const welcomeText = document.querySelector('.round-outcome > p');
-    const roundOutComeText = document.querySelector('.round-outcome-text > p');
-    const randomText = [
-        'Dare you challenge me?',
-        'Bring it on!',
-        'How about a game of rock paper scissors?',
-        'I\'m not rigged, I swear!'
-    ]
+  const score = document.querySelector('.score > p');
+  const welcomeText = document.querySelector('.round-outcome > p');
+  const roundOutComeText = document.querySelector('.round-outcome-text > p');
+  const randomText = [
+    'Dare you challenge me?',
+    'Bring it on!',
+    'How about a game of rock paper scissors?',
+    'I\'m not rigged, I swear!',
+  ];
 
-    score.innerHTML = '0 vs 0';
-    welcomeText.innerHTML = randomText[getRandomInt(0, randomText.length)];
-    roundOutComeText.innerHTML = ' ';
+  score.innerHTML = '0 vs 0';
+  welcomeText.innerHTML = randomText[getRandomInt(0, randomText.length)];
+  roundOutComeText.innerHTML = ' ';
 }
 
-export {toggleButtons, getRandomInt, initializeText, generateEmoji}
+export {
+  toggleButtons,
+  getRandomInt,
+  initializeText,
+  generateEmoji,
+};
